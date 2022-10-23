31b7ca41a5733ba786b384b14c37af07cf37c2e3
hexzedels <hexpisos@gmail.com>
2022-10-23 15:33:50 +0300

    Docker-compose works with postgres.

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
-DATABASE_URL = "postgresql://test_user:test_password@94.45.223.241:46873/test"
+from os import environ
+
+
+# os.environ['VAR_NAME']
+DATABASE_URL = environ['DATABASE_URL'] # "postgresql://test_user:test_password@94.45.223.241:46873/test"
